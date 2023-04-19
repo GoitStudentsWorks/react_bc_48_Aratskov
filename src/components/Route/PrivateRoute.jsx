56568b8f00ba68260cb764596802b38c42fc11c5
Andrii <ifromukraine2022@gmail.com>
2023-04-20 01:04:31 +0300

    /

--- src/components/Route/PrivateRoute.jsx
+++ src/components/Route/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { getAuthStatus } from 'redux/Auth/authSelectors';
 
-export const PrivateRoute = ({ component, redirectTo = '/login' }) => {
+export const PrivateRoute = ({ component, redirectTo = '/' }) => {
   const isAuth = useSelector(getAuthStatus);
 
   return isAuth ? component : <Navigate to={redirectTo} />;
