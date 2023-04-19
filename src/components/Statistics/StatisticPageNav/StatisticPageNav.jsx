21cd0c8d4f35c5cc3f64d93edc10d60206ef6541
Andrii <ifromukraine2022@gmail.com>
2023-04-19 07:37:50 +0300

    add navigate on statisticks

--- src/components/Statistics/StatisticPageNav/StatisticPageNav.jsx
+++ src/components/Statistics/StatisticPageNav/StatisticPageNav.jsx
@@ -1,11 +1,15 @@
-import CategoriesList from '../CategoriesList/CategoriesList';
-import ExpensesList from '../ExpensesList/ExpensesList';
+// import CategoriesList from '../CategoriesList/CategoriesList';
+// import ExpensesList from '../ExpensesList/ExpensesList';
+import { NavLink } from 'react-router-dom';
 
 const StatisticPageNav = () => {
   return (
     <>
-      <ExpensesList />
-      <CategoriesList />
+      {/* <ExpensesList /> */}
+      {/* <CategoriesList /> */}
+      <NavLink to="transactions">Expenses</NavLink>
+      <NavLink to="categories">Categories</NavLink> 
+      
     </>
   );
 };
--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,11 +1,14 @@
 import Statistics from 'components/Statistics/Statistics';
+import { Outlet } from 'react-router-dom';
 
 const StatisticsPage = () => {
   return (
     <>
       <Statistics />
+      <Outlet />
     </>
   );
 };
 
 export default StatisticsPage;
+
