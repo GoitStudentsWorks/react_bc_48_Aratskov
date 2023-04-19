bce5dff14faa4e62130486596df6874b6d3f3664
dyonns <117724779+dyonns@users.noreply.github.com>
2023-04-19 21:41:41 +0300

    layout/content

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,14 @@
 import MonthPeriod from './MonthPeriod/MonthPeriod';
 import StatisticPageNav from './StatisticPageNav/StatisticPageNav';
+import s from './Statistics.module.css';
 
 const Statistics = () => {
   return (
-    <>
+    <div className={s.style}>
       <StatisticPageNav />
       <MonthPeriod />
-    </>
+    </div>
   );
 };
 
 export default Statistics;
-      
