863a6f436a9842ff30664690f184b80bea99b0ab
Trehub Anatolii <tridfoto@gmail.com>
2023-04-21 00:07:35 +0300

    homepage almost ready

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,14 @@ import manMobileX from 'assets/img/man_mob.png';
 import manMobileXx from 'assets/img/man_mob-2x.png';
 import st from './HomePage.module.scss';
 
+const BluredItem = ({ children }) => {
+  return (
+    <>
+      <div className={st.bluredWrapper}>{children}</div>
+    </>
+  );
+};
+
 const HomePage = () => {
   return (
     <>
@@ -16,21 +24,48 @@ const HomePage = () => {
           <span className={st.text}>savings for an apartment</span>
         </h1>
       </div>
-      <picture className={st.pic}>
-        <source
-          srcSet={`${manDesktopX} 1x, ${manDesktopXx} 2x`}
-          media="(min-width: 1280px)"
-        />
-        <source
-          srcSet={`${manTabletX} 1x, ${manTabletXx} 2x`}
-          media="(min-width: 768px)"
-        />
-        <source
-          srcSet={`${manMobileX} 1x, ${manMobileXx} 2x`}
-          media="(max-width: 767px)"
-        />
-        <img src={manDesktopX} alt="logo man" className={st.image} />
-      </picture>
+
+      <div className={st.imageBlock}>
+        <div className={st.picWrapper}>
+          <picture className={st.pic}>
+            <source
+              srcSet={`${manDesktopX} 1x, ${manDesktopXx} 2x`}
+              media="(min-width: 1280px)"
+            />
+            <source
+              srcSet={`${manTabletX} 1x, ${manTabletXx} 2x`}
+              media="(min-width: 768px)"
+            />
+            <source
+              srcSet={`${manMobileX} 1x, ${manMobileXx} 2x`}
+              media="(max-width: 767px)"
+            />
+            <img src={manDesktopX} alt="logo man" className={st.image} />
+          </picture>
+
+          <div className={st.bluredLeft}>
+            <BluredItem>
+              <p>
+                Payments that work{' '}
+                <span style={{ color: '#3A6AF5', fontWeight: '700' }}>
+                  for you
+                </span>
+              </p>
+            </BluredItem>
+          </div>
+
+          <div className={st.bluredRight}>
+            <BluredItem>
+              <p>
+                <span style={{ color: '#3A6AF5', fontWeight: '700' }}>
+                Apartments
+                </span>
+                of your dreams
+              </p>
+            </BluredItem>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
