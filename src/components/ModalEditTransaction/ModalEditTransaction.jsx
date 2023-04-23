61bb504e62013754b4ab2a01fc26022c3e6a6b19
adventuretimedi <olhadianaaa@gmail.com>
2023-04-23 21:53:55 +0300

    Fix in select in modal transaction, adding NotFound page

--- src/components/ModalEditTransaction/ModalEditTransaction.jsx
+++ src/components/ModalEditTransaction/ModalEditTransaction.jsx
@@ -4,6 +4,9 @@ import Input from '../Input/Input';
 import s from './ModalEditTransaction.module.scss';
 import { useState } from 'react';
 import { SelectWithLabel } from 'components/SelectWithLabel/SelectWithLabel';
+import svg from '../../assets/icons/sprite.svg';
+import { useSelector } from 'react-redux';
+import { selectCategoriesWithIcons } from 'redux/Cashflow/cashflowSelectors';
 
 export const ModalEditTransaction = ({ show, onClose }) => {
   // eslint-disable-next-line no-unused-vars
@@ -14,7 +17,8 @@ export const ModalEditTransaction = ({ show, onClose }) => {
   ]);
   const [comment, setComment] = useState('Bag');
   const [sum, setSum] = useState(1500);
-  const [selectedOption, setSelectedOption] = useState(category[0]);
+  // const [selectedOption, setSelectedOption] = useState(category[0]);
+  const categories = useSelector(selectCategoriesWithIcons);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -24,11 +28,19 @@ export const ModalEditTransaction = ({ show, onClose }) => {
     onClose();
   };
 
-  const handleChange = selectedOption => {
-    console.log('selectedOption:', selectedOption);
-    setSelectedOption(selectedOption);
-  };
+  const [selectedCategory, setSelectedCategory] = useState({
+    name: 'other',
+    title: 'Other',
+    icon: `${svg}#icon-settings`,
+  });
 
+  // const handleChange = selectedOption => {
+  //   console.log('selectedOption:', selectedOption);
+  //   setSelectedOption(selectedOption);
+  // };
+  const handleCategoryChange = category => {
+    setSelectedCategory(category);
+  };
   return (
     <Modal
       show={show}
@@ -39,11 +51,13 @@ export const ModalEditTransaction = ({ show, onClose }) => {
       <form action="" onSubmit={handleSubmit}>
         <div className={s.inputWrapper}>
           <SelectWithLabel
+            name="category"
+            value={selectedCategory}
+            options={categories}
             label="Per category"
-            value={selectedOption}
-            options={category}
-            onChange={handleChange}
+            onChange={handleCategoryChange}
           />
+
           <Input
             label="Expense comment"
             value={comment}
