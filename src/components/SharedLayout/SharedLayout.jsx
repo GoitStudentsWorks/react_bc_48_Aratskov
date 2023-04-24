ab2bf36094686e764954fb89249215b84d04142c
Andrii <ifromukraine2022@gmail.com>
2023-04-24 13:52:02 +0300

    init

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,18 +1,21 @@
 import Header from 'components/Header/Header';
 import ModalBurger from 'components/ModalBurger/ModalBurger';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const SharedLayout = () => {
-  const burg = useSelector(state => state.burger);
+  const burg = useSelector(state=>state.burger)
   return (
     <>
       <Header />
-      <main>
-        {!burg && <Outlet />}
-        {burg && <ModalBurger />}
-      </main>
-      <footer></footer>
+      <Suspense fallback={null}>
+        <main >
+          {!burg&&<Outlet />}
+          {burg&&<ModalBurger />}
+        </main>
+        <footer></footer>
+      </Suspense>
     </>
   );
 };
