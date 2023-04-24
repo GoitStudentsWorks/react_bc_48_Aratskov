c3f0d92b28e2a1371490c08ac303ec09a92f49f7
Andrii <ifromukraine2022@gmail.com>
2023-04-24 13:46:37 +0300

    delete suspence

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,21 +1,18 @@
 import Header from 'components/Header/Header';
 import ModalBurger from 'components/ModalBurger/ModalBurger';
-import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const SharedLayout = () => {
-  const burg = useSelector(state=>state.burger)
+  const burg = useSelector(state => state.burger);
   return (
     <>
       <Header />
-      <Suspense fallback={<div style={{color:"#f3f3f3", textAlign:"center"}}>Loading..</div>}>
-        <main >
-          {!burg&&<Outlet />}
-          {burg&&<ModalBurger />}
-        </main>
-        <footer></footer>
-      </Suspense>
+      <main>
+        {!burg && <Outlet />}
+        {burg && <ModalBurger />}
+      </main>
+      <footer></footer>
     </>
   );
 };
